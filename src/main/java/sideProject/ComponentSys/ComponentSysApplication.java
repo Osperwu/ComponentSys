d1d41b36290674c979d25053cb419d4a4663e51d
Osper Wu <timwu6@gmail.com>
2023-07-18 13:42:51 +0800

    init springboot

---
+++ src/main/java/sideProject/ComponentSys/ComponentSysApplication.java
@@ -0,0 +1,13 @@
+package sideProject.ComponentSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sideProject/ComponentSys/ComponentSysApplicationTests.java
@@ -0,0 +1,13 @@
+package sideProject.ComponentSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
